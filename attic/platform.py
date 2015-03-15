3cab20cc5326d0be2267c5c271bbde4887ad90a1
galets <sam@galets.net>
2015-03-15 16:08:05 -0700

    Update platform.py

--- attic/platform.py
+++ attic/platform.py
@@ -14,28 +14,30 @@ elif platform.startswith('CYGWIN'):
     API_VERSION = 2
 
     def acl_get(path, item, st, numeric_owner=False):
-        try:
-            # Using non-numeric names, i.e., group names, has caused problems.
-            # Hence the -n option
-            ACL_text = subprocess.check_output(['getfacl.exe', '-n', path])
-            item[b'acl_access'] = ACL_text
-        except:
-            pass
+        return
+        #try:
+            ## Using non-numeric names, i.e., group names, has caused problems.
+            ## Hence the -n option
+            #ACL_text = subprocess.check_output(['getfacl.exe', '-n', path])
+            #item[b'acl_access'] = ACL_text
+        #except:
+            #pass
 
     def acl_set(path, item, numeric_owner=False):
-        try:
-            ACL_Access = item[b'acl_access']
-        except:
-            return 
+        return
+        #try:
+            #ACL_Access = item[b'acl_access']
+        #except:
+            #return 
         
-        buf = io.StringIO(ACL_Access.decode('utf-8'))
+        #buf = io.StringIO(ACL_Access.decode('utf-8'))
 
-        for line in buf:
-            if len(line.strip()) > 0 and (not line.strip().startswith('#')):
-                param = ['setfacl.exe', '-m', line.rstrip(), path]
-                retVal = subprocess.call(param)
-                if retVal != 0:
-                    raise Exception('ACLs not set successfully')
+        #for line in buf:
+            #if len(line.strip()) > 0 and (not line.strip().startswith('#')):
+                #param = ['setfacl.exe', '-m', line.rstrip(), path]
+                #retVal = subprocess.call(param)
+                #if retVal != 0:
+                    #raise Exception('ACLs not set successfully')
                                       
 else:
     API_VERSION = 1
